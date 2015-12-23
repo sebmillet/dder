7ac5e664fe6993a50355fa7b1565c8c8a1c00316
Sébastien Millet <sebastien.millet1@club-internet.fr>
2015-12-23 14:14:18 +0100

    dder passed to 'autoconf - automake' standard

---
+++ extracfg.h
@@ -0,0 +1,24 @@
+/* Name of package */
+#define PACKAGE "dder"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "milletseb@laposte.net"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "dder"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "dder 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "dder"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL ""
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
+
+/* Version number of package */
+#define VERSION "0.1"
+
